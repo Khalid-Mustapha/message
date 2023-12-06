99516df3dce8aa4e7612c3dcc4c21f272898b223
Khalid Mustapha <111358006+Khalid-Mustapha@users.noreply.github.com>
2023-12-06 14:17:50 +0100

    Update script.js

--- Message/script.js
+++ Message/script.js
@@ -5,7 +5,10 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "Obviously!!!🤭";
+  question.innerHTML = "
+    Obviously!!!🤭
+    I love you more ...
+    ";
   gif.src =
     "https://media.giphy.com/media/UMon0fuimoAN9ueUNP/giphy.gif";
 });
